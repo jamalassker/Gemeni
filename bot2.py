58eb8dc73461ca02851747272b9b71b150b61abe
jamalassker <jamalassker2032@gmail.com>
2026-01-08 15:58:35 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -31,7 +31,7 @@ class PersistentProSniper:
         self.models = {}
         self.positions = {}
         # STATE VARIABLES - Persistent throughout the session
-        self.cash = 10
+        self.cash = 100
         self.realized_pnl = 0.0
         self.total_fees = 0.0
         self.report_id = None
